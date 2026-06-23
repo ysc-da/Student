22dff25cabeeed0e26774f7f55ba1e37f54e0b82
userName <2822030907@qq.com>
2026-06-23 22:25:52 +0800

    添加了程序入口，背景图片

---
+++ src/main/java/com/expense/main/MainApp.java
@@ -0,0 +1,18 @@
+package com.expense.main;
+
+import com.expense.ui.MainFrame;
+
+import javax.swing.*;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // 窗口初始化与显示逻辑
+                MainFrame frame = new MainFrame();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
